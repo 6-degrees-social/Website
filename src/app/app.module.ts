e39aa8e8d14986204674793d5bc51900d6d1b2a4
Brian Keaveney <briankeaveney@gmail.com>
2019-04-16 16:00:56 +0100

    navbar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NewsletterFormComponent } from './newsletter-form/newsletter-form.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NewsletterFormComponent
+    NewsletterFormComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
